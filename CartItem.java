552a708c19c5b9549953296fd653d81f1cff20af
MANZA <36519974+zaimanza@users.noreply.github.com>
2021-01-21 01:23:57 +0800

    CartItem

---
+++ CartItem.java
@@ -0,0 +1,38 @@
+
+public class CartItem {
+    public String itemID;
+    public String itemName;
+    public String itemPrice;
+    public String itemStock;
+    public String itemDesc;
+    public String staffID;
+    public String CategoryName;
+
+    public void setCartId(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setItemPrice(String itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public void setItemStock(String itemStock) {
+        this.itemStock = itemStock;
+    }
+
+    public void setItemDesc(String itemDesc) {
+        this.itemDesc = itemDesc;
+    }
+
+    public void setStaffId(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public void setCategoryName(String CategoryName) {
+        this.CategoryName = CategoryName;
+    }
+}
